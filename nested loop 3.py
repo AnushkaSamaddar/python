f9ad322a23f22a47a818c164958950cd65608b32
AnushkaSamaddar <anushkaschool30@gmail.com>
2026-07-22 23:36:29 +0530

    Add files via upload

---
+++ nested loop 3.py
@@ -0,0 +1,16 @@
+n=3
+for i in range(1,n+1):
+    print(" "*(n-i),end="")
+    print("* "*i)
+for i in range(n-1,0,-1):
+    print(" "*(n-i),end="")
+    print("* "*i)    
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()        
+for i in range(1,n):
+    for j in range(n-i):
+        print("* ",end="")
+    print()  
+         
